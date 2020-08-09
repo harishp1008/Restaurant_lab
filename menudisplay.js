854fc05e93a38511fb2be62efd585db114535bd3
harishp1008 <perumalpharish@gmail.com>
2020-08-09 19:25:28 +0530

    xml

--- menudisplay.js
+++ menudisplay.js
@@ -1,4 +1,4 @@
-  <script>
+  
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -145,4 +145,3 @@ function myFunction6(xml) {
     }
     document.getElementById("Desert").innerHTML = table;
 }
-</script>
